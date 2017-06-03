2b36f9a3c30e6e7b57e38c8e5c34271a82fb0135
elfiservice <armandojunior@Armandos-MacBook-Pro.local>
2017-06-03 09:40:37 -0300

    implement Workers and fix bug imageManips/worker func

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -1,5 +1,9 @@
 (function(){
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
+
+  //instance of Worker, new threads
+  var myWorker = new Worker('scripts/worker.js');
+
   var original;
   var imageLoader = document.querySelector('#imageLoader');
   imageLoader.addEventListener('change', handleImage, false);
@@ -43,20 +47,18 @@
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    //send To new thread the tow vars. to be manipulated and give back
+      myWorker.postMessage({"imageData":imageData,"type":type});
+      console.log('Message posted to worker');
+
+      // receiving the response of the thread myWorker
+      myWorker.onmessage = function(e) {
+        toggleButtonsAbledness();
+        var image = e.data;
+        if (image) {
+          return ctx.putImageData(e.data, 0, 0);
+        }
+      };
   };
 
   function revertImage() {
